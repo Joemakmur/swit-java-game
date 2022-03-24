4e80c82c9a45f621ebf7b67d943b8fcefb862e04
Joemakmur <97925135+Joemakmur@users.noreply.github.com>
2022-03-24 08:40:43 +0700

    Add files via upload

---
+++ game tangan/Javascript/js.js
@@ -0,0 +1,137 @@
+function getPilihanComputer() {
+    // membangkitkan bilangan random
+    const comp = Math.random();
+
+    if( comp < 0.34 ) return 'gajah';
+    if( comp >= 0.34 && comp < 0.67 )return'orang';
+        return'semut';
+}
+
+function getHasil(comp, player) {
+     // menentukan hasil
+    if( player == comp ) return 'SERI!';
+    if( player == 'gajah' ) return ( comp == 'orang' ) ? 'MENANG!' : 'KALAH!';
+    if( player == 'orang' ) return( comp == 'gajah' ) ? 'KALAH!' : 'MENANG!';
+    if( player == 'semut' ) return( comp == 'orang' ) ? 'KALAH' : 'MENANG!';
+    } 
+
+
+function putar(){
+    const imgComputer = document.querySelector('.img-komputer')
+    const gambar = ['gajah', 'semut', 'orang']
+    let i = 0
+    const waktuMulai = new Date().getTime()
+    setInterval(function(){
+        if( new Date().getTime() - waktuMulai > 1000){
+            clearInterval
+            return;
+        }
+        imgComputer.setAttribute('src', 'img/' + gambar[i++] +'.png')
+        if (i == gambar.length) 
+            i = 0
+    }, 100)
+}
+
+// nb: interval 100 mean 0,1 detik
+
+
+
+
+const pilihan = document.querySelectorAll('li img')
+pilihan.forEach(function(pil) {
+    pil.addEventListener('click', function(){
+            const pilihanComputer = getPilihanComputer()
+    const pilihanPlayer = pil.className
+    const hasil = getHasil(pilihanComputer, pilihanPlayer)
+    // console.log('comp : ' + pilihanComputer)
+    // console.log('player : ' + pilihanPlayer)
+
+
+
+    putar()
+
+    setTimeout(function(){
+    const imgComputer = document.querySelector('.img-komputer')
+    imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png')
+
+
+    const info = document.querySelector('.info')
+    info.innerHTML = hasil
+    }, 1000)
+
+    })
+})
+
+
+
+
+// // mulai masuk eventnya
+
+
+
+// const pGajah = document.querySelector('.gajah')
+
+// // lalu kita kasih event
+
+// pGajah.addEventListener('click', function() {
+//     const pilihanComputer = getPilihanComputer()
+//     const pilihanPlayer = pGajah.className
+//     const hasil = getHasil(pilihanComputer, pilihanPlayer)
+//     // console.log('comp : ' + pilihanComputer)
+//     // console.log('player : ' + pilihanPlayer)
+
+
+
+//     const imgComputer = document.querySelector('.img-komputer')
+//     imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png')
+
+
+//     const info = document.querySelector('.info')
+//     info.innerHTML = hasil
+
+// })
+
+
+// const pOrang = document.querySelector('.orang')
+
+// // lalu kita kasih event
+
+// pOrang.addEventListener('click', function() {
+//     const pilihanComputer = getPilihanComputer()
+//     const pilihanPlayer = pOrang.className
+//     const hasil = getHasil(pilihanComputer, pilihanPlayer)
+//     // console.log('comp : ' + pilihanComputer)
+//     // console.log('player : ' + pilihanPlayer)
+
+
+
+//     const imgComputer = document.querySelector('.img-komputer')
+//     imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png')
+
+
+//     const info = document.querySelector('.info')
+//     info.innerHTML = hasil
+
+// })
+
+// const pSemut = document.querySelector('.semut')
+
+// // lalu kita kasih event
+
+// pSemut.addEventListener('click', function() {
+//     const pilihanComputer = getPilihanComputer()
+//     const pilihanPlayer = pOrang.className
+//     const hasil = getHasil(pilihanComputer, pilihanPlayer)
+//     // console.log('comp : ' + pilihanComputer)
+//     // console.log('player : ' + pilihanPlayer)
+
+
+
+//     const imgComputer = document.querySelector('.img-komputer')
+//     imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png')
+
+
+//     const info = document.querySelector('.info')
+//     info.innerHTML = hasil
+
+// })
